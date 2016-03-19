88df66b01b304d4a3230c87400ba27c0ec43a3bb
Muhd Syuqri <d.syuqri@hotmail.com>
2016-03-19 12:46:14 +0800

    - added instructionstate to be used for instructions for game

---
+++ core/src/com/mygdx/game/states/InstructionState.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Syuqri on 3/19/2016.
+ */
+public class InstructionState extends State{
+    public InstructionState(GameStateManager gsm){
+        super(gsm);
+    }
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
