329e02c346060efdc56356f1e46fe97743f0df8a
Muhd Syuqri <d.syuqri@hotmail.com>
2016-03-19 10:30:08 +0800

    - added specific location for button press (not anywhere on the screen anymore)

--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -3,6 +3,7 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by Syuqri on 3/7/2016.
@@ -10,19 +11,27 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MenuState extends State{
     private Texture background;
     private Texture playBtn;
+    private Vector3 touchPos = new Vector3(0,0,0);
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
         background = new Texture("bg.png");
         playBtn = new Texture("playBtn.png");
+
     }
 
     @Override
     public void handleInput() {
-        if(Gdx.input.justTouched()){
-            System.out.println("This is menustate");
-            gsm.set(new PlayState(gsm));
-            dispose();
+        if(Gdx.input.isTouched()){
+            touchPos.x = Gdx.input.getX();
+            touchPos.y = Gdx.input.getY();
+            if(touchPos.x<=(Gdx.graphics.getWidth()/2)+(playBtn.getWidth()*3/2) && touchPos.x>=(Gdx.graphics.getWidth()/2)-(playBtn.getWidth()*3/2)){
+                if(touchPos.y<=(Gdx.graphics.getHeight()/2)+(playBtn.getHeight()*3/2) && touchPos.y>=(Gdx.graphics.getHeight()/2)-(playBtn.getHeight()*3/2)){
+                    System.out.println("This is menustate");
+                    gsm.set(new PlayState(gsm));
+                    dispose();
+                }
+            }
         }
     }
 
@@ -35,7 +44,7 @@ public class MenuState extends State{
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        sb.draw(playBtn,(Gdx.graphics.getWidth()/2)-(playBtn.getWidth()/2),Gdx.graphics.getHeight()/2);
+        sb.draw(playBtn,(Gdx.graphics.getWidth()/2)-(playBtn.getWidth()*3/2),Gdx.graphics.getHeight()/2,playBtn.getWidth()*3,playBtn.getHeight()*3);
         sb.end();
     }
 
