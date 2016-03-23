eb415b67292ab5545d772862327998613b5cdea1
Muhd Syuqri <d.syuqri@hotmail.com>
2016-03-23 21:09:57 +0800

    **********CHANGES FROM MASTER********** - added RestartState( currently empty)

---
+++ core/src/com/mygdx/game/states/RestartState.java
@@ -0,0 +1,33 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Syuqri on 3/23/2016.
+ */
+public class RestartState extends State{
+
+    public RestartState(GameStateManager gsm){
+        super(gsm);
+
+    }
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
