6f1e876a81ac4762fcc7020659b2118229a960e2
Muhd Syuqri <d.syuqri@hotmail.com>
2016-03-28 13:25:16 +0800

    ********CHANGES FROM MASTER ************** - Changed bg and effects size - Added files via upload

--- core/src/com/mygdx/game/customEnum/MapTile.java
+++ core/src/com/mygdx/game/customEnum/MapTile.java
@@ -5,5 +5,5 @@ package com.mygdx.game.customEnum;
  * Created by Samuel on 23/3/2016.
  */
 public enum MapTile {
-    EMPTY, OBSTACLES, POWER, DOOR, SWITCH;
+    EMPTY, OBSTACLES, POWER, DOOR, SWITCH, SPIKES;
 }
--- core/src/com/mygdx/game/objects/Overlay.java
+++ core/src/com/mygdx/game/objects/Overlay.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public class Overlay extends Movable{
     public Overlay(float y){
-        super(0, y, 480, 800);
+        super(0, y, 480, 200);
         this.setImage(new Texture(Gdx.files.internal("effects1.png")));
     }
 }
