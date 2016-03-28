56c9ab90aff3bed6ac8b900ae223d00f4c714aa8
Samuel <samuel_limjh@mymail.sutd.edu.sg>
2016-03-28 10:51:36 +0800

    Neaten the wallCoord Reduced frequency of spikes

--- core/src/com/mygdx/game/states/PlayStateHost.java
+++ core/src/com/mygdx/game/states/PlayStateHost.java
@@ -14,8 +14,7 @@ import java.util.HashSet;
  * Created by hj on 19/3/16.
  */
 public class PlayStateHost extends PlayState {
-    private int doorCounter;
-    private int powerCounter;
+    private int doorCounter, powerCounter, spikeCounter;
     private ArrayList<MapTile[]> memory;
 
     public PlayStateHost(GameStateManager gsm){
@@ -23,6 +22,7 @@ public class PlayStateHost extends PlayState {
         //spawning initialization
         doorCounter = 0;
         powerCounter = 0;
+        spikeCounter = 0;
         memory = new ArrayList<MapTile[]>();
         MapTile[] init = {MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY, MapTile.EMPTY};
         for (int i = 0; i < 5; i++){
@@ -32,18 +32,12 @@ public class PlayStateHost extends PlayState {
         mapMaker.start();
     }
 
-    void wallCoord(){
-        powerCounter += 1;
-        doorCounter += 1;
+    public MapTile[] generator(MapTile[] new_row){
         boolean test = false;
-        int out_index = 0;
-        MapTile[] new_row = createArray(MapTile.OBSTACLES);
-
-        // random generator
         while (!test) {
             int temp = MathUtils.random(1, 9);
             for (int i = 0; i < temp; i++) {
-                int coord = MathUtils.random(0,8);
+                int coord = MathUtils.random(0, 8);
                 new_row[coord] = MapTile.EMPTY;
             }
             for (int i = 0; i < new_row.length; i++) {
@@ -53,8 +47,11 @@ public class PlayStateHost extends PlayState {
                 }
             }
         }
+        return new_row;
+    }
 
-        // updating the path array (only 1 path)
+    public boolean[] updatePath(MapTile[] new_row, boolean[] current){
+        int out_index = 0;
         for (int k = 0; k < new_row.length; k++) {
             if ((new_row[k] == MapTile.EMPTY) && current[k]) {
                 current[k] = true;
@@ -64,8 +61,6 @@ public class PlayStateHost extends PlayState {
             }
         }
 
-        // updating the path array (if there are walkable areas next to the true path then they are
-        // also true)
         for (int j = 1; j < new_row.length; j++) {
             if (out_index + j < new_row.length) {
                 if ((new_row[out_index + j] == MapTile.EMPTY) && current[out_index + j - 1]) {
@@ -84,35 +79,69 @@ public class PlayStateHost extends PlayState {
                 }
             }
         }
+        return current;
+    }
 
-        // creating random spikes
-        int spikeNo = MathUtils.random(0,3);
+    public MapTile[] genSpikes(MapTile[] new_row){
+        int spikeNo = MathUtils.random(0,2);
         for (int j = 0; j < spikeNo; j++) {
             int pos = MathUtils.random(0,8);
             if (new_row[pos] == MapTile.OBSTACLES){
                 new_row[pos] = MapTile.SPIKES;
             }
         }
+        return new_row;
+    }
+
+    public MapTile[] genPower(MapTile[] new_row){
+        while (true){
+            int temp = MathUtils.random(0,8);
+            if (new_row[temp] == MapTile.EMPTY){
+                new_row[temp] = MapTile.POWER;
+                powerCounter = 0;
+                break;
+            }
+        }
+        return new_row;
+    }
+
+    public MapTile[] genDoor(MapTile[] new_row){
+        for (int i = 0; i < current.length; i++) {
+            if (new_row[i] == MapTile.EMPTY) {
+                new_row[i] = MapTile.DOOR;
+            }
+        }
+        return new_row;
+    }
+
+    void wallCoord(){
+        powerCounter += 1;
+        doorCounter += 1;
+        spikeCounter += 1;
+        boolean test = false;
+        int out_index = 0;
+        MapTile[] new_row = createArray(MapTile.OBSTACLES);
+
+        // random generator
+        new_row = generator(new_row);
+
+        // updating the path array
+        current = updatePath(new_row, current);
+
+        // creating random spikes
+        if (spikeCounter > 3) {
+            new_row = genSpikes(new_row);
+            spikeCounter = 0;
+        }
 
         // spawning power ups after a certain time. 20 is default. 5 is for testing
         if (powerCounter > 5){
-            while (true){
-                int temp = MathUtils.random(0,8);
-                if (new_row[temp] == MapTile.EMPTY){
-                    new_row[temp] = MapTile.POWER;
-                    powerCounter = 0;
-                    break;
-                }
-            }
+            new_row = genPower(new_row);
         }
 
         // spawning door
         if (doorCounter > 44){
-            for (int i = 0; i < current.length; i++) {
-                if (new_row[i] == MapTile.EMPTY) {
-                    new_row[i] = MapTile.DOOR;
-                }
-            }
+            new_row = genDoor(new_row);
             doorCounter = 0;
         }
 
