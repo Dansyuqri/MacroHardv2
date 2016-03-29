abfeb121f3b5662c4f875d303d591c6d17c9449b
Muhd Syuqri <d.syuqri@hotmail.com>
2016-03-29 18:45:49 +0800

    - Added CustomButton class - rearranged menustate buttons code (shortened)

---
+++ core/src/com/mygdx/game/objects/CustomButton.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.objects;
+
+/**
+ * Created by Syuqri on 3/29/2016.
+ */
+public class CustomButton extends GameObject{
+    
+    public CustomButton(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+}
