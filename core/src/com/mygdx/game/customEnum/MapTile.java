5bd0b7e6faee1e95cabbba7966b803750efad9df
hj <hanjie_sng@mymail.sutd.edu.sg>
2016-03-29 14:47:03 +0800

    made byte coder and decoder for maptile

--- core/src/com/mygdx/game/customEnum/MapTile.java
+++ core/src/com/mygdx/game/customEnum/MapTile.java
@@ -5,4 +5,22 @@ package com.mygdx.game.customEnum;
  */
 public enum MapTile {
     EMPTY, OBSTACLES, POWER, DOOR, SWITCH, SPIKES;
+
+    public byte toByte(){
+        for (int i = 0; i < values().length; i++) {
+            if (this.equals(values()[i])){
+                return (byte) i;
+            }
+        }
+        return (byte) -1;
+    }
+
+    public MapTile fromByte(byte b){
+        for (int i = 0; i < values().length; i++) {
+            if (b == (byte) i){
+                return values()[i];
+            }
+        }
+        return EMPTY;
+    }
 }
