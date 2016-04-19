6ff58c0440f9a34cf4ccab94eb59da377c7fee41
hj <hanjie_sng@mymail.sutd.edu.sg>
2016-04-19 16:25:58 +0800

    testing power up

--- core/src/com/mygdx/game/customEnum/PowerType.java
+++ core/src/com/mygdx/game/customEnum/PowerType.java
@@ -4,6 +4,6 @@ package com.mygdx.game.customEnum;
  * Created by MinhBreaker on 23/3/16.
  */
 public enum PowerType {
-    NOTHING, FREEZE_MAZE, SLOW_GAME_DOWN, SPEED_PLAYER_UP, TELEPORT,
-    DESTROY_WALL, INVINCIBLE
+    NOTHING, FREEZE_MAZE, SLOW_GAME_DOWN, SPEED_PLAYER_UP, INVINCIBLE,
+    DESTROY_WALL, TELEPORT
 }
